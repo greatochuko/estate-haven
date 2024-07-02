6bd461ab42dd44d6e8f452e58cb776864ec23472
Great <greatochuko123@gmail.com>
2024-07-02 22:43:10 +0100

    create hero section

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const navLinks = [
+  { name: "Home", href: "/" },
+  { name: "Property", href: "/property" },
+  { name: "Agents", href: "/agents" },
+];
+
+export default function Header() {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex justify-between items-center py-4 font-bold">
+      <h1>Estate Haven</h1>
+      <ul className="flex items-center gap-4">
+        {navLinks.map((navLink) => (
+          <li key={navLink.name}>
+            <Link
+              href={navLink.href}
+              className={`p-2 hover:text-accent-green-200 duration-300 ${
+                pathname === navLink.href ? "text-accent-green-100" : ""
+              }`}
+            >
+              {navLink.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="flex items-center gap-4">
+        <Link
+          href={"/login"}
+          className="px-2 py-1 text-accent-green-100 hover:text-accent-green-200"
+        >
+          Login
+        </Link>
+        <Link
+          href={"/signup"}
+          className="bg-accent-green-100 hover:bg-accent-green-200 px-2 py-1 rounded-md text-white duration-300"
+        >
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import heroImage from "../../public/hero-image.png";
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <div className="relative mb-20">
+      <div className="relative flex bg-zinc-100 rounded-xl w-full max-h-[30rem] overflow-hidden aspect-[1.5] sm:aspect-video">
+        <div className="top-0 left-o absolute w-full h-full">
+          <div className="flex flex-col justify-center gap-2 p-10 sm:w-[50%] h-full">
+            <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+              Find a modern property that suits you
+            </h2>
+            <p className="font-semibold text-sm text-zinc-500 md:text-base lg:text-lg">
+              Our platform offers the best opportunity to find the best real
+              estate possible. We can make this process easy and comfortable
+            </p>
+            <button className="bg-accent-green-100 px-4 p-2 rounded-lg w-fit font-bold text-sm text-white sm:text-base">
+              Add Listing
+            </button>
+          </div>
+        </div>
+        <Image
+          src={heroImage}
+          alt="Hero image"
+          className="ml-auto w-[50%] object-cover"
+        ></Image>
+      </div>
+      <form className="bottom-0 left-[50%] absolute flex justify-between items-center gap-4 bg-white shadow-[0_0_5px] shadow-zinc-200 mx-auto p-8 rounded-2xl w-[90%] max-w-4xl -translate-x-[50%] translate-y-[50%]">
+        <div className="flex flex-col gap-2">
+          <label className="font-bold" htmlFor="location">
+            Location
+          </label>
+          <input
+            type="text"
+            id="location"
+            name="location"
+            placeholder="Enter your city name"
+            className="border-[3px] p-2 rounded-md focus-visible:ring ring-accent-green-100"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label className="font-bold" htmlFor="property-type">
+            Property Type
+          </label>
+          <select
+            name="property-type"
+            id="property-type"
+            className="border-[3px] p-2 rounded-md focus-visible:ring ring-accent-green-100"
+          >
+            <option value="apartment">Apartment</option>
+            <option value="bumgalow">Bungalow</option>
+            <option value="duplex">Duplex</option>
+            <option value="town house">Town house</option>
+          </select>
+        </div>
+        <div className="flex flex-col gap-2">
+          <label className="font-bold" htmlFor="">
+            Price Range
+          </label>
+          <div className="flex items-center gap-2">
+            <input
+              type="number"
+              name="from-price"
+              id="from-price"
+              placeholder="0"
+              className="border-[3px] p-2 rounded-md w-28 focus-visible:ring ring-accent-green-100"
+            />
+            -
+            <input
+              placeholder="any"
+              type="number"
+              name="to-price"
+              id="to-price"
+              className="border-[3px] p-2 rounded-md w-28 focus-visible:ring ring-accent-green-100"
+            />
+          </div>
+        </div>
+        <button className="bg-accent-green-100 hover:bg-accent-green-200 p-4 rounded-full duration-300">
+          <svg
+            width={30}
+            height={30}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M16.6725 16.6412L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
+                stroke="#fff"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>{" "}
+            </g>
+          </svg>
+        </button>
+      </form>
+    </div>
+  );
+}
