7f50173e4172cef7a7483d2642c4934563efb7f0
Great <greatochuko123@gmail.com>
2024-07-03 20:24:16 +0100

    create newsLetter submit function

--- src/components/NewsLetterSection.tsx
+++ src/components/NewsLetterSection.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 export default function NewsLetterSection() {
+  const [email, setEmail] = useState("");
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setEmail("");
+  }
+
   return (
     <div className="flex flex-col justify-center items-center gap-4 md:gap-6 lg:gap-8 bg-accent-green-100 p-4 sm:p-8 rounded-xl min-h-72 text-center text-white aspect-[3]">
       <h1 className="font-bold text-[min(max(1.4rem,_4vw),_2.5rem)]">
@@ -11,11 +19,17 @@ export default function NewsLetterSection() {
         Market Trends, and Expert Real Estate Advice Delivered Straight to Your
         Inbox
       </p>
-      <form className="relative flex w-full max-w-[30rem]">
+      <form
+        className="relative flex w-full max-w-[30rem]"
+        onSubmit={handleSubmit}
+      >
         <input
-          type="text"
+          type="email"
           className="flex-1 bg-white p-4 pr-28 sm:pr-28 rounded-xl text-zinc-800"
           placeholder="Enter your email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <button
           type="submit"
