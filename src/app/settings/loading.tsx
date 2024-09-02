44091f7169b5702c799c348b624b483801f58a4a
Great <greatochuko123@gmail.com>
2024-09-02 18:19:40 +0100

    add loading page to settings page

---
+++ src/app/settings/loading.tsx
@@ -0,0 +1,7 @@
+import LoadingScreen from "@/components/LoadingScreen";
+import Rating from "@/components/Rating";
+import React from "react";
+
+export default function loading() {
+  return <LoadingScreen />;
+}
