11d49e5c9aa6e8a2c03a379dcf85a0be506bd90c
Great <greatochuko123@gmail.com>
2024-12-03 10:08:59 +0100

    redesign footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { locations } from "./PropertiesByLocationSection";
 
 const socialLinks = [
-  { name: "Facebook", url: "#" },
   { name: "LinkedIn", url: "https://linkedin.com/in/greatochuko" },
   { name: "Twitter", url: "https://x.com/greatochuko123" },
   { name: "GitHub", url: "https://github.com/greatochuko" },
@@ -16,15 +15,15 @@ export default function Footer() {
   });
 
   return (
-    <footer className="bg-zinc-100 py-16 font-semibold text-sm text-zinc-600 sm:text-base">
+    <footer className="bg-zinc-100 py-16 font-medium text-sm text-zinc-600">
       <div className="gap-4 grid grid-cols-2 lg:grid-cols-4 mx-auto px-4 w-[95%] sm:w-[90%] max-w-7xl">
         <div className="flex flex-col gap-2 w-48">
-          <h3 className="font-bold text-xl text-zinc-900">Estate Haven</h3>
+          <h3 className="font-semibold text-lg text-zinc-900">Estate Haven</h3>
           <p>1234 Example Road Suite 567 Lagos, NG 100001 Nigeria</p>
           <p>greatochuko123@gmail.com</p>
         </div>
         <div className="flex flex-col justify-self-end sm:justify-self-auto gap-2 w-32 sm:w-48">
-          <h3 className="font-bold text-xl text-zinc-900">Company</h3>
+          <h3 className="font-semibold text-lg text-zinc-900">Company</h3>
           <ul className="flex flex-col">
             <li>
               <Link
@@ -61,12 +60,15 @@ export default function Footer() {
           </ul>
         </div>
         <div className="flex flex-col gap-2 w-48">
-          <h3 className="font-bold text-xl text-zinc-900">Cities</h3>
+          <h3 className="font-semibold text-lg text-zinc-900">Cities</h3>
           <ul className="flex flex-col">
             {cities.map((city) => (
               <li key={city}>
                 <Link
-                  href={`/properties?city=${city}`}
+                  href={`/properties?city=${city
+                    .split(" ")
+                    .join("-")
+                    .toLowerCase()}`}
                   className="block p-1 hover:text-accent-green-100 duration-300"
                 >
                   {city}
@@ -76,7 +78,7 @@ export default function Footer() {
           </ul>
         </div>
         <div className="flex flex-col justify-self-end sm:justify-self-auto gap-2 w-32 sm:w-48">
-          <h3 className="font-bold text-xl text-zinc-900">Social</h3>
+          <h3 className="font-semibold text-lg text-zinc-900">Social</h3>
           <ul className="flex flex-col">
             {socialLinks.map((socialLink) => (
               <li key={socialLink.name}>
